606fa360d19f226364c4ae66b483d856456c186f
XiangyuWu1995 <59391046+XiangyuWu1995@users.noreply.github.com>
2020-01-04 11:10:58 +0800

    Update image.cpp

--- image.cpp
+++ image.cpp
@@ -15,20 +15,20 @@ struct S_IJ
 {
 	unsigned int I;
 	unsigned int J;
-	bool operator == (const S_IJ & obj) const //重载 “==” 操作符，函数最后的 const 别忘了，否则会报错。
+	bool operator == (const S_IJ & obj) const 
 	{
-		return I == obj.I && J == obj.J;//具体匹配条件，可以自己设定
+		return I == obj.I && J == obj.J;
 	}
 
-	bool operator<(const struct S_IJ & right)const   //重载<运算符
+	bool operator<(const struct S_IJ & right)const
 	{
-		if (this->I == right.I&&this->J == right.J)     //根据id去重
+		if (this->I == right.I&&this->J == right.J)
 			return false;
 		else
 		{
 			if (this->I != right.I)
 			{
-				return this->I < right.I;      //升序
+				return this->I < right.I;
 			}
 			else
 			{
@@ -38,7 +38,7 @@ struct S_IJ
 	}
 };
 
-//一个点坐标结构
+
 struct S_XY
 {
 	double X;
@@ -46,13 +46,13 @@ struct S_XY
 
 };
 
-//存储单个网格四个顶点坐标
+
 struct grid_XY
 {
 	vector<S_XY>vxy;
 };
 
-//按I从小到大排序
+
 bool comp_I(const S_IJ &a, const S_IJ &b)
 {
 	if (a.I < b.I)
@@ -64,7 +64,7 @@ bool comp_I(const S_IJ &a, const S_IJ &b)
 }
 
 
-//按J从小到大排序
+
 bool comp_J(const S_IJ &a, const S_IJ &b)
 {
 	if (a.J < b.J)
@@ -82,14 +82,13 @@ void multiscale_combine(vector<uint64> vOut, vector<grid_XY>&xy)
 	uint64 outMc0;
 
 
-	//新添加
+
 	vector<S_IJ>vij;
 	vector<S_XY>vxy;
 	vector<S_IJ>points_IJ;
 	vector<S_XY>polygon_XY;
 	vector<S_IJ>::iterator ite;
 
-	//将所有顶点装入vector，舍弃出现偶数次的顶点
 	for (int i = 0; i < vOut.size(); i++)
 	{
 		outMc0 = vOut[i];
@@ -113,7 +112,7 @@ void multiscale_combine(vector<uint64> vOut, vector<grid_XY>&xy)
 
 
 
-	//将顶点按x排序
+
 	sort(points_IJ.begin(), points_IJ.end(), comp_I);
 	vector<S_IJ>sorted_I;
 	for (ite = points_IJ.begin(); ite != points_IJ.end(); ite++)
@@ -122,7 +121,7 @@ void multiscale_combine(vector<uint64> vOut, vector<grid_XY>&xy)
 	}
 
 
-	//将顶点按y排序
+
 	sort(points_IJ.begin(), points_IJ.end(), comp_J);
 	vector<S_IJ>sorted_J;
 	for (ite = points_IJ.begin(); ite != points_IJ.end(); ite++)
@@ -131,7 +130,7 @@ void multiscale_combine(vector<uint64> vOut, vector<grid_XY>&xy)
 	}
 
 
-	//将水平边装进字典
+
 	map<S_IJ, S_IJ>edges_h;
 	int i = 0;
 	while (i < sorted_J.size())
@@ -144,8 +143,6 @@ void multiscale_combine(vector<uint64> vOut, vector<grid_XY>&xy)
 			i += 2;
 		}
 	}
-
-	//将垂直边装进字典
 	map<S_IJ, S_IJ>edges_v;
 	int j = 0;
 	while (j < sorted_I.size())
@@ -160,27 +157,27 @@ void multiscale_combine(vector<uint64> vOut, vector<grid_XY>&xy)
 	}
 
 
-	//得到边界坐标串
+
 	int e = 0;
 	vector<S_IJ>polygon_IJ;
 	grid_XY qq;
 	while (edges_h.size() > 0)
 	{
 		polygon_IJ.clear();
-		polygon_IJ.push_back((*edges_h.begin()).first);//把水平边第一个点装入IJ坐标串
+		polygon_IJ.push_back((*edges_h.begin()).first);
 		edges_h.erase(polygon_IJ.back());
 		polygon_IJ.push_back(edges_v[polygon_IJ.back()]);
 		edges_v.erase(polygon_IJ.back());
 		e = 1;
 		while (polygon_IJ.back().I != polygon_IJ.front().I || polygon_IJ.back().J != polygon_IJ.front().J)
 		{
-			if (e == 0)//e=0则从垂直边中取点
+			if (e == 0)
 			{
 				polygon_IJ.push_back(edges_v[polygon_IJ.back()]);
 				edges_v.erase(polygon_IJ.back());
 				e = 1;
 			}
-			else //e=0则从水平边中取点
+			else
 			{
 				polygon_IJ.push_back(edges_h[polygon_IJ.back()]);
 				edges_h.erase(polygon_IJ.back());
